e5dc665591f7e5def6f0f1022052dbf750f3a107
Sylvain <sylvain.brichet@citopia.fr>
2022-03-16 17:31:44 +0100

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Checkbox from "./components/Checkbox";
+import { OPTIONS } from "./Constants";
 
 function App() {
+  const [checkboxes, setCheckboxes] = useState(
+    OPTIONS.reduce(
+      (options, option) => ({
+        ...options,
+        [option]: false
+      }),
+      {}
+  ))
+  const [allselected, setAllselected] = useState(true)
+  
+  const selectAllCheckboxes = (isSelected, isClicked) => {
+    Object.keys(checkboxes).forEach(checkbox => {
+      setCheckboxes(prevState => ({
+        ...prevState,
+        [checkbox]: isSelected
+      }))
+    })
+    setAllselected(isClicked)
+  }
+  
+  const handleCheckboxChange = changeEvent => {
+    const { name } = changeEvent.target;
+    setCheckboxes({
+      ...checkboxes,
+      [name]: !checkboxes[name]
+    })
+  }
+
+  const createCheckbox = option => (
+    <Checkbox
+      label={option}
+      isSelected={checkboxes[option]}
+      onCheckboxChange={handleCheckboxChange}
+      key={option}
+    />
+  );
+
+  const createCheckboxes = () => OPTIONS.map(createCheckbox);
+
+  const allSelectStyle = {
+    padding: 12,
+    borderRadius: 6,
+    marginBottom: 6,
+  }
+
+  const unSelectStyle = {
+    ...allSelectStyle,
+    color: 'white',
+    backgroundColor: 'lightslategrey'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: 15}}>
+      {allselected
+        ? <button style={allSelectStyle} onClick={() => selectAllCheckboxes(true, false)}>Select all</button>
+        : <button style={unSelectStyle} onClick={() => selectAllCheckboxes(false, true)}>Unselect All</button>
+      }
+      {createCheckboxes()}
     </div>
   );
 }
---
+++ src/Constants.js
@@ -0,0 +1,9 @@
+export const OPTIONS = [
+    "Pyjama",
+    "Chaussette",
+    "Panthere", 
+    "Allumette",
+    "Ampoule",
+    'Item 3',
+    'Item 99'
+];
